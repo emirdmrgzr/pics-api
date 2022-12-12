eb940351d3b23253c12aac87ef4d16032e3349aa
emirdmrgzr <emir.demirgezer@hotmail.com>
2022-12-12 21:36:53 +0300

    x

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -6,7 +6,7 @@ function SearchBar({ onSubmit }) {
   const handleFormSubmit = (event) =>{
     event.preventDefault();
 
-    onSubmit({term}.value);
+    onSubmit({term});
   }
 
   const handleChange = (event) =>{
